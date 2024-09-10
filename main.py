6cd8a1c4f41eb359f10499664c3df33d9c4521ab
robinsoj <robinsoj1228@gmail.com>
2024-09-10 15:54:04 -0700

    Player class and actions added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 	print("Starting asteroids!")
@@ -10,13 +11,25 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	black = (0, 0, 0)
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	updateable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers= (updateable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(black)
+		for item in updateable:
+			item.update(dt)
+		for item in drawable:
+			item.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
